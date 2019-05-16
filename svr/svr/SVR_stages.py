94762a831934b2667800c1a935bb1ac6b4a1b3e0
Krish <skesineni@deakin.edu.au>
2019-05-16 12:16:25 +1000

    SVR

--- svr/svr/SVR_stages.py
+++ svr/svr/SVR_stages.py
@@ -39,11 +39,16 @@ class FeedData(Stage):
 
 
 class SVRData(SurroundData):
+
+    dates = []
+    prices = []
+
     def __init__(self):
         self.dta = pd.DataFrame()
 
+
     def get_data(self):
-        self.dta = pd.read_csv('/Users/saikrishna/Documents/GitHub/Surround_AI_Suqad_2/svr/svr/config.yaml')
+        self.dta = pd.read_csv('config.yaml')
 
 
 
@@ -56,21 +61,21 @@ class ComputeForecast(SurroundData, Stage):
 
     def predict_price(self, dates, prices):
 
-        '''
-        	Builds predictive model and graphs it
-        	This function creates 3 models, each of them will be a type of support vector machine.
-        	A support vector machine is a linear seperator. It takes data that is already classified and tries
-        	to predict a set of unclassified data.
-        	So if we only had two data classes it would look like this
-        	It will be such that the distances from the closest points in each of the two groups is farthest away.
-        	When we add a new data point in our graph depending on which side of the line it is we could classify it
-        	accordingly with the label. However, in this program we are not predicting a class label, so we don't
-        	need to classify instead we are predicting the next value in a series which means we want to use regression.
-        	SVM's can be used for regression as well. The support vector regression is a type of SVM that uses the space between
-        	data points as a margin of error and predicts the most likely next point in a dataset.
-        	The predict_prices returns predictions from each of our models
+        """
+        Builds predictive model and graphs it
+        This function creates 3 models, each of them will be a type of support vector machine.
+        A support vector machine is a linear seperator. It takes data that is already classified and tries
+        to predict a set of unclassified data.
+        So if we only had two data classes it would look like this
+        It will be such that the distances from the closest points in each of the two groups is farthest away.
+        When we add a new data point in our graph depending on which side of the line it is we could classify it
+        accordingly with the label. However, in this program we are not predicting a class label, so we don't
+        need to classify instead we are predicting the next value in a series which means we want to use regression.
+        SVM's can be used for regression as well. The support vector regression is a type of SVM that uses the space between
+        data points as a margin of error and predicts the most likely next point in a dataset.
+        The predict_prices returns predictions from each of our models
 
-        	'''
+        """
 
         dates = np.reshape(dates, (len(dates), 1)) # converts to a matrix from a vector
         # Linear support vector regression model.
@@ -98,18 +103,19 @@ class ComputeForecast(SurroundData, Stage):
         print(svr_lin.predict(dates))
         print(svr_poly.predict(dates))
 
+
     def operate(self, surround_data, config):
-        dates = []
-        prices = []
-        s_data = surround_data.dta
-        with open('/Users/saikrishna/Documents/GitHub/Surround_AI_Suqad_2/svr/data/AAPL.csv', 'r') as csvfile:
+
+        svr_data = surround_data.dta
+        file_path = config.get_path("surround.path")
+        with open(file_path, 'r') as csvfile:
             # csvFileReader allows us to iterate over every row in our csv file
             csvFileReader = csv.reader(csvfile)
             next(csvFileReader)  # skipping column names
             for row in csvFileReader:
-                dates.append(int(row[0].split('-')[0]))  # Only gets day of the month which is at index 0
-                prices.append(float(row[1])) # Convert to float for more precision
-                self.predict_price(dates, prices)
+                surround_data.dates.append(int(row[0].split('-')[0]))  # Only gets day of the month which is at index 0
+                surround_data.prices.append(float(row[1])) # Convert to float for more precision
+                self.predict_price(surround_data.dates, surround_data.prices)
 
 
                 return
@@ -118,10 +124,11 @@ class ComputeForecast(SurroundData, Stage):
 
 class PlotResult(SurroundData, Stage):
 
+
     def __init__(self):
         self.surround_data = pd.DataFrame()
 
-    def plot(self):
+    def plot(self, dates, prices):
         plt.scatter(dates, prices, color='black', label='Data')  # plotting the initial datapoints
         # The graphs are plotted with the help of SVR object in scikit-learn using the dates matrix as our parameter.
         # Each will be a distinct color and and give them a distinct label.
@@ -139,7 +146,7 @@ class PlotResult(SurroundData, Stage):
 
     def operate(self, surround_data, config):
 
-         self.plot()
+         self.plot(dates,prices)
          return
 
 
